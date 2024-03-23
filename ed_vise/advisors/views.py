41d884a9829aaefe8d243a110adb3717936bd84d
lindsaylian26 <112498774+lindsaylian26@users.noreply.github.com>
2024-03-23 12:19:31 -0400

    made views for advisors

--- ed_vise/advisors/views.py
+++ ed_vise/advisors/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AdvisorForm
+from .models import Advisor
 
-# Create your views here.
+def register_advisor(request):
+    if request.method == 'POST':
+        form = AdvisorForm(request.POST)
+        if form.is_valid():
+            advisor = form.save(commit=False)
+            advisor.user = request.user
+            advisor.save()
+            return redirect('advisor_profile', advisor_id=advisor.id)
+    else:
+        form = AdvisorForm()
+    return render(request, 'advisors/register.html', {'form': form})
+
+def advisor_profile(request, advisor_id):
+    advisor = Advisor.objects.get(id=advisor_id)
+    return render(request, 'advisors/profile.html', {'advisor': advisor})
