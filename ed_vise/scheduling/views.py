e559f6ecf53107f95a4ae897ff3c6563aeddebc9
lindsaylian26 <112498774+lindsaylian26@users.noreply.github.com>
2024-03-23 17:09:42 -0400

    added oauth initialization to views

--- ed_vise/scheduling/views.py
+++ ed_vise/scheduling/views.py
@@ -1,6 +1,27 @@
+"""
 from django.shortcuts import render
 from .models import Event
 
 def event_list(request):
     events = Event.objects.all()
     return render(request, 'calendar_app/event_list.html', {'events': events})
+"""
+
+
+from google_auth_oauthlib.flow import Flow
+from django.shortcuts import redirect
+
+def google_login(request):
+    flow = Flow.from_client_secrets_file(
+        'client_secrets.json',
+        scopes=['https://www.googleapis.com/auth/calendar'],
+        redirect_uri='http://localhost:8000/oauth2callback'
+    )
+
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        include_granted_scopes='true'
+    )
+
+    request.session['state'] = state
+    return redirect(authorization_url)
