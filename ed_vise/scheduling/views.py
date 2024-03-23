54459f8822bc16eff4498c071580dfd4515d76c1
lindsaylian26 <112498774+lindsaylian26@users.noreply.github.com>
2024-03-23 16:43:38 -0400

    views.py for calendar

--- ed_vise/scheduling/views.py
+++ ed_vise/scheduling/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Event
 
-# Create your views here.
+def event_list(request):
+    events = Event.objects.all()
+    return render(request, 'calendar_app/event_list.html', {'events': events})
