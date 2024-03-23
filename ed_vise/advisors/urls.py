e0fa9a300382a0cb158448bc0f3288e5ef1be9ad
tashajeyasingh <tashajeyasingh@gmail.com>
2024-03-23 17:58:09 -0400

    API

--- ed_vise/advisors/urls.py
+++ ed_vise/advisors/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import AdvisorList
 
 urlpatterns = [
     path('register/', views.register_advisor, name='register_advisor'),
     path('profile/<int:advisor_id>/', views.advisor_profile, name='advisor_profile'),
+    path('api/advisors/', AdvisorList.as_view(), name='advisor-list')
+
 ]
