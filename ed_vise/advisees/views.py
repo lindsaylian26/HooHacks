8ae902b0bd4651ef929a6691de7ed3193e71a0c7
lindsaylian26 <112498774+lindsaylian26@users.noreply.github.com>
2024-03-23 12:29:56 -0400

    created templates for advisees and advisors

--- ed_vise/advisees/views.py
+++ ed_vise/advisees/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AdviseeForm
+from .models import Advisee
 
-# Create your views here.
+def register_advisee(request):
+    if request.method == 'POST':
+        form = AdviseeForm(request.POST)
+        if form.is_valid():
+            advisee = form.save(commit=False)
+            advisee.user = request.user
+            advisee.save()
+            return redirect('advisee_profile', advisee_id=advisee.id)
+    else:
+        form = AdviseeForm()
+    return render(request, 'advisees/register.html', {'form': form})
+
+def advisee_profile(request, advisee_id):
+    advisee = Advisee.objects.get(id=advisee_id)
+    return render(request, 'advisees/profile.html', {'advisee': advisee})
