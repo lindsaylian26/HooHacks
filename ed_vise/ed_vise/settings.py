5df2b62dd97ead45e770256035986902346207dd
lindsaylian26 <112498774+lindsaylian26@users.noreply.github.com>
2024-03-23 14:20:17 -0400

    fixed views

--- ed_vise/ed_vise/settings.py
+++ ed_vise/ed_vise/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'advisors',
     'advisees',
+    'ed_vise',
 ]
 
 MIDDLEWARE = [
--- ed_vise/ed_vise/views.py
+++ ed_vise/ed_vise/views.py
@@ -1,6 +1,7 @@
 # ed_vise/views.py
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to the Ed-Vise homepage!")
+    return render(request, 'ed_vise/index.html')
