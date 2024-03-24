d7f51056456d058388b2d25058db57f26e4ffba8
lindsaylian26 <112498774+lindsaylian26@users.noreply.github.com>
2024-03-24 00:30:30 -0400

    added advisor to admin side

--- ed_vise/advisees/admin.py
+++ ed_vise/advisees/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import SubjectArea  # Adjust the import path as needed
+from .models import SubjectArea, Advisor  # Adjust the import path as needed
 
 @admin.register(SubjectArea)
 class SubjectAreaAdmin(admin.ModelAdmin):
+    list_display = ['name']  
+
+
+@admin.register(Advisor)
+class AdvisorAdmin(admin.ModelAdmin):
     list_display = ['name']  
